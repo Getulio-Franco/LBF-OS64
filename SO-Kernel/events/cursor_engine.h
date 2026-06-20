518ea2dc59188c8c8fe2b8f5a524e1d5743c452e
Getulio <franco_advocacia@outlook.com>
2026-06-20 20:27:58 -0300

    Delete SO-Kernel directory

--- SO-Kernel/events/cursor_engine.h
+++
@@ -1,8 +0,0 @@
-#ifndef CURSOR_ENGINE_H
-#define CURSOR_ENGINE_H
-
-void cursor_restore_bg(int mx, int my, int screen_w, int screen_h);
-void cursor_save_bg(int mx, int my, int screen_w, int screen_h);
-void cursor_draw(int mx, int my, int screen_w, int screen_h);
-
-#endif
