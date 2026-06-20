8b730ff509d8ad6fc137d4cab099fbaeee821556
Getulio <franco_advocacia@outlook.com>
2026-06-20 20:21:43 -0300

    Delete SO-Kernel directory

--- SO-Kernel/Runtime_IA/iasynth.h
+++
@@ -1,6 +0,0 @@
-#ifndef IASYNTH_H
-#define IASYNTH_H
-
-void iasynth_gerar_resposta(const char* prompt_original, const char* raw_result, char* buffer_saida);
-
-#endif
--- SO-Kernel/events/mouse_geometry.h
+++
@@ -1,6 +0,0 @@
-#ifndef MOUSE_GEOMETRY_H
-#define MOUSE_GEOMETRY_H
-
-void geometry_apply_limits(int *x, int *y, int screen_w, int screen_h);
-
-#endif
